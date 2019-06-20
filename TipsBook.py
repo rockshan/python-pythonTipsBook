6ef1702866abdbeb821ff56f6b35f770a938206e
rockshan <jha.nitt.cse@gmail.com>
2019-06-19 22:58:34 -0700

    Update TipsBook.py

--- TipsBook.py
+++ TipsBook.py
@@ -1,3 +1,13 @@
 def apply_discount(product, discount):
-  price = int(product['price'] * (1.0 - discount)) assert 0 <= price <= product['price']
+  price = int(product['price'] * (1.0 - discount)) 
+  assert 0 <= price <= product['price']
   return price
+
+#adder factiory
+def make_adder(n): 
+  def add(x):
+    return x + n 
+  return add
+
+plus_3 = make_adder(3)
+plus_5 = make_adder(5)
